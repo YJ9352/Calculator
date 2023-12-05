672a4882e95731094b667e7d34d9bb1241faf259
YJ9352 <150547656+YJ9352@users.noreply.github.com>
2023-12-05 17:55:49 +0900

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.calculator_study
+
+fun main(args: Array<String>) {
+
+    println("숫자를 입력해주세요.")
+    var num1 = readLine()!!.toDouble()
+
+    println("연산자를 넣어주세요(+, -, /, *)")
+    val operator = readLine()!!.toString()
+
+    println("숫자를 입력해주세요.")
+    var num2 = readLine()!!.toDouble()
+
+    var calculator = Calculator()
+    var result = calculator.oprator(num1, num2, operator)
+
+    println("정답은 ${result}입니다.")
+    Info(num1, num2, operator).endinfo()
+
+}
